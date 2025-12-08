91c3fb4c62dc37fc2307d7d34387f83aad6796a1
Zalan12 <farkaszalan@turr.hu>
2025-12-08 11:26:52 +0100

    HON-6 PageNotFound komponens létrehozása

--- honfoglalasWeb/src/app/app.routes.ts
+++ honfoglalasWeb/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NotfoundComponent } from './components/system/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "/main", pathMatch: 'full'},
+    {path: '**', component:NotfoundComponent}
+    
+];
