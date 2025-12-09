c285c1824169fd065e083ca01d1a5983b5074316
Zalan12 <farkaszalan@turr.hu>
2025-12-09 10:29:47 +0100

    HON-9 Főoldal adatbeolvasás adatbázisból

--- honfoglalasWeb/src/app/components/system/main/main.component.ts
+++ honfoglalasWeb/src/app/components/system/main/main.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../../../services/message.service';
+import { APIservice } from '../../../services/api.service';
+import { Accommodation } from '../../../interfaces/accommodation';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent  implements OnInit{
 
+  
+  accomms:Accommodation[]=[]
+
+  constructor(
+    private Api: APIservice,
+    private mess: MessageService
+  ){}
+
+ngOnInit(): void {
+  this.getHotels();
+}
+getHotels(){
+  this.Api.SelectAll('accommodations').then(res =>{
+    this.accomms = res.data;
+    console.log(this.accomms);
+  })
+}
 }
---
+++ honfoglalasWeb/src/app/interfaces/accommodation.ts
@@ -0,0 +1,10 @@
+export interface Accommodation{
+    id?:number,
+    name:string,
+    descrip:string,
+    address:string,
+    capacity:number,
+    basePrice:number,
+    active:boolean,
+    createdAt:string
+}
