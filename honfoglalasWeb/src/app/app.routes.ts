8aaed66f728fc4678bfd06268395607be49a9e13
Zalan12 <farkaszalan@turr.hu>
2025-12-09 08:29:39 +0100

    HON-8 Backend feltöltése

--- honfoglalasWeb/src/app/app.routes.ts
+++ honfoglalasWeb/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotfoundComponent } from './components/system/notfound/notfound.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
+import { MainComponent } from './components/system/main/main.component';
 
 export const routes: Routes = [
     {path: 'registration', component:RegistrationComponent},
-
+    {path:'main',component:MainComponent},
     {path: '', redirectTo: "/main", pathMatch: 'full'},
     {path: '**', component:NotfoundComponent}
     
